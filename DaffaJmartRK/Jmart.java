80f1776f7f5210bef8059a5915b7d8f7b6dfec58
Muhammad Daffa Ajiputra <muhammad.daffa99@ui.ac.id>
2021-09-11 11:01:21 +0700

    Modul 1

--- DaffaJmartRK/Jmart.java
+++ DaffaJmartRK/Jmart.java
@@ -9,27 +9,36 @@ package DaffaJmartRK;
  */
 public class Jmart
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Jmart
-     */
-    public Jmart()
-    {
-        // initialise instance variables
-        x = 0;
+    public static void main (String[] args){
+        
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public static int getPromo(){
+        return 0;
+    }
+    public static String getCustomer(){
+        String exit = "oop";
+        return exit;
+    }
+    public float getDiscountPercentage(int before,int after){
+        return 0;
+    }
+    public int getDiscountPrice(int price,float discountPercentage){
+        if(discountPercentage > 100.0){
+            discountPercentage = 100;
+        }
+        float hargaBarang = price - (price*(discountPercentage/100));
+        return price;
+    }
+    public int getOriginialPrice(int discountedPrice,float discountPercentage){
+        return 0;
+    }
+    public float getCommissionMultiplier(){
+        return (float)0.05;
+    }
+    public int getAdjustedPrice(int price){
+        return 0;
+    }
+    public int getAdminFee(int price){
+        return 0;
     }
 }
