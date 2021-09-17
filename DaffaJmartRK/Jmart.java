2d58e1c723ae4b5caa8068023d5180614861bb4a
Muhammad Daffa Ajiputra <muhammad.daffa99@ui.ac.id>
2021-09-17 19:17:07 +0700

    Modul 1 revisi

--- DaffaJmartRK/Jmart.java
+++ DaffaJmartRK/Jmart.java
@@ -10,7 +10,15 @@ package DaffaJmartRK;
 public class Jmart
 {
     public static void main (String[] args){
-        
+        Jmart cetak = new Jmart();
+        System.out.println(cetak.getPromo());
+        System.out.println(cetak.getCustomer());
+        System.out.println(cetak.getDiscountPercentage(1000, 500));
+        System.out.println(cetak.getDiscountPrice(1000, 10.0f));
+        System.out.println(cetak.getOriginalPrice(900, 10.0f));
+        System.out.println(cetak.getCommissionMultiplier());
+        System.out.println(cetak.getAdjustedPrice(1000));
+        System.out.println(cetak.getAdminFee(1000));
     }
     public static int getPromo(){
         return 0;
@@ -19,26 +27,36 @@ public class Jmart
         String exit = "oop";
         return exit;
     }
-    public float getDiscountPercentage(int before,int after){
-        return 0;
+    public static float getDiscountPercentage(int before,int after){
+        float discount;
+        discount = 100 * ((float) (before - after)/(float)before);
+        if(before < after){
+            return 0.0f;
+        }
+        return discount;
     }
     public int getDiscountPrice(int price,float discountPercentage){
         if(discountPercentage > 100.0){
-            discountPercentage = 100;
+            discountPercentage = 100.0f;
         }
-        float hargaBarang = price - (price*(discountPercentage/100));
-        return price;
+        float hargaBarang = (int) ((price * (100.0f - discountPercentage)/ 100.0f));
+        return (int)hargaBarang;
     }
-    public int getOriginialPrice(int discountedPrice,float discountPercentage){
-        return 0;
+    public int getOriginalPrice(int discountedPrice,float discountPercentage){
+        float hargaAwal = (int) ((discountedPrice * 100.0f) / (100.0f - discountPercentage));
+        return (int)hargaAwal;
     }
     public float getCommissionMultiplier(){
         return (float)0.05;
     }
     public int getAdjustedPrice(int price){
-        return 0;
+        float commision = getCommissionMultiplier();
+        price = (price + (int) (commision * (float) price));
+        return price;
     }
     public int getAdminFee(int price){
-        return 0;
+        float admin = getCommissionMultiplier();
+        float fee = admin * (int) price;
+        return (int) fee;
     }
 }
