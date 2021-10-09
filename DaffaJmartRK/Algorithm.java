b8a6a6326990e64e458fa377adad33469b5d55eb
Muhammad Daffa Ajiputra <muhammad.daffa99@ui.ac.id>
2021-10-09 10:50:34 +0700

    CS Modul 4

---
+++ DaffaJmartRK/Algorithm.java
@@ -0,0 +1,70 @@
+package DaffaJmartRK;
+import java.util.*;
+
+public class Algorithm {
+	private Algorithm(){
+		
+	}
+	public static <T> int count(T[] Array, T value){
+		return 0;
+	}
+	
+	public static <T> int count(Iterable<T>iterable, T value) {
+		return 0;
+	}
+	
+	public static <T> int count(Iterator<T>iterator, T value) {
+		return 0;
+	}
+	public static <T> int count(T[] Array, Predicate<T>pred) {
+		return 0;
+	}
+	public static <T> int count(Iterable<T>iterable, Predicate<T>pred) {
+		return 0;
+	}
+	public static <T> int count(Iterator<T>iterator, Predicate<T>pred) {
+		return 0;
+	}
+	public static <T> boolean exists(T[] Array, T value) {
+		return true;
+	}
+	public static <T> boolean exists(Iterable<T> iterable, T value) {
+		return true;
+	}
+	public static <T> boolean exists(Iterator<T> iterator, T value) {
+		return true;
+	}
+	public static <T> boolean exists(T[] Array, Predicate<T>pred) {
+		return true;
+	}
+	public static <T> boolean exists(Iterable<T> iterable, Predicate<T>pred) {
+		return true;
+	}
+	public static <T> boolean exists(Iterator<T> iterator, Predicate<T>pred) {
+		return true;
+	}
+	public static <T> T find(T[] Array, T value) {
+		return null;
+	}
+	public static <T> T find(Iterable<T> iterable, T value) {
+		return null;
+	}
+	public static <T> T find(Iterator<T> Array, T value) {
+		return null;
+	}
+	public static <T> T find(T[] Array, Predicate<T>pred) {
+		return null;
+	}
+	public static <T> T find(Iterable<T> iterable, Predicate<T>pred) {
+		return null;
+	}
+	public static <T> T find(Iterator<T> Array, Predicate<T>pred) {
+		return null;
+	}
+	public static <T> T max(T first, T second){
+		return null;
+	}
+	public static <T> T min(T first, T second) {
+		return null;
+	}
+} 
---
+++ DaffaJmartRK/Predicate.java
@@ -0,0 +1,6 @@
+package DaffaJmartRK;
+
+@FunctionalInterface
+public interface Predicate<T> {
+	public boolean predicate(T Arg);
+}
