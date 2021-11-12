2b549f49d45bcc65105d7ac17ce47b12d3d6d749
Muhammad Daffa Ajiputra <muhammad.daffa99@ui.ac.id>
2021-11-12 22:43:07 +0700

    Revisi Modul 6

--- DaffaJmartRK/Jmart.java
+++ DaffaJmartRK/Jmart.java
@@ -16,14 +16,36 @@ class Jmart
     	return paginate(list, page, pageSize, predicate);
     }
     public static List<Product> filterByCategory(List<Product> list, ProductCategory category){
-    	return null;
+    	List<Product> filtered = new ArrayList<>();
+    	for(Product product : list) {
+    		if(product.category.equals(category)) filtered.add(product);
+    	}
+    	return filtered;
     }
     public static List<Product> filterByName(List<Product> list, String search, int page, int pageSize){
     	Predicate<Product> predicate = (tmp -> (tmp.name.toLowerCase().contains(search.toLowerCase())));
         return paginate(list, page, pageSize, predicate);
     }
     public static List<Product> filterByPrice(List<Product> list, double MinPrice, double MaxPrice){
-    	return null;
+    	List<Product> filtered = new ArrayList<>();
+    	for(int i = 0; i < list.size(); i++){
+            if(MinPrice <= 0.0){
+                if(list.get(i).price <= MaxPrice){
+                    filtered.add(list.get(i));
+                }
+            }
+            else if(MaxPrice <= 0.0){
+                if(list.get(i).price >= MinPrice){
+                    filtered.add(list.get(i));
+                }
+            }
+            else{
+                if(list.get(i).price >= MinPrice && list.get(i).price <= MaxPrice){
+                    filtered.add(list.get(i));
+                }
+            }
+        }
+        return filtered;
     }
     public static void main(String[] args) {
     	/*System.out.println("account id:" + new Account(null, null, null, -1).id);
@@ -36,21 +58,21 @@ class Jmart
     	
     	try {
     		List<Product> list = read("/Java OOP/Praktikum OOP/jmart/json/randomProductList.json");
-    		List<Product> filtered = filterByPrice(list, 0.0, 20000.0);
-    		filtered.forEach(product -> System.out.println(product.price));
+    		
+    		/*List<Product> filtered = filterByPrice(list, 0.0, 20000.0);
+    		filtered.forEach(product -> System.out.println(product.price));*/
+    		List<Product> filteredByName = filterByName(list, "gtx", 1, 5);
+    		filteredByName.forEach(product -> System.out.println(product.name));
+    		List<Product> filteredByAccountId = filterByAccountId(list, 1, 0, 5);
+    		filteredByAccountId.forEach(product -> System.out.println(product.name));
     	}
     	catch(Throwable t){
     		t.printStackTrace();
     	}
     }
     private static List<Product> paginate (List<Product> list, int page, int pageSize, 	Predicate<Product> pred){
-    	if(pageSize <= 0 || page <= 0) throw new IllegalArgumentException("Invalid Page Size: " + pageSize);
-    	
-    	int index = (page - 1) * pageSize;
-    	if(list == null || list.size() <= index){
-    		return Collections.emptyList();
-    	}
-    	return list.stream().filter(temp -> pred.predicate(temp)).skip(index).limit(pageSize).collect(Collectors.toList());
+    	if(pageSize < 0 || page < 0) throw new IllegalArgumentException("Invalid Page Size: " + pageSize);
+    	return list.stream().filter(tmp -> pred.predicate(tmp)).skip(page * pageSize).limit(pageSize).collect(Collectors.toList());
     	
     }
     public static List<Product> read(String filepath){
